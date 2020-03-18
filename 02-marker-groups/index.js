f0dd0b0f9720fa1aa01a303ba03f3fa12295bb7a
kunxin-chor <chorkunxin@yahoo.com>
2020-03-18 08:39:54 +0000

    Made some misc. changes

---
+++ 02-marker-groups/index.js
@@ -0,0 +1,32 @@
+function getRandomLatLng(map) {
+    var bounds = map.getBounds(),
+        southWest = bounds.getSouthWest(),
+        northEast = bounds.getNorthEast(),
+        lngSpan = northEast.lng - southWest.lng,
+        latSpan = northEast.lat - southWest.lat;
+
+    return L.latLng(
+            southWest.lat + latSpan * Math.random(),
+            southWest.lng + lngSpan * Math.random());
+}
+
+// setup the map
+let singapore = [ 1.29,103.85]; // Singapore latlng
+let map = L.map('map').setView(singapore, 13)
+
+// create the tile layer
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw' //demo access token
+}).addTo(map);
+
+var markers = L.markerClusterGroup();
+for (let i=0; i<1000;i++)
+{
+    markers.addLayer(L.marker(getRandomLatLng(map)));
+}
+	map.addLayer(markers);
