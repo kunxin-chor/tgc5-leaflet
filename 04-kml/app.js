d645d8b18bfda6dccf4211aa503da9dad9cf4011
kunxin-chor <chorkunxin@yahoo.com>
2020-03-24 07:56:35 +0000

    Added demo for KML files

--- 04-kml/app.js
+++ 04-kml/app.js
@@ -1,6 +1,10 @@
 $(function() {
 
     let map = createMap("map", [1.3521, 103.8198], 13);
-
+    var runLayer = omnivore.kml('data/cycling-path-network-kml.kml')
+    .on('ready', function() {
+        map.fitBounds(runLayer.getBounds());
+    })
+    .addTo(map);
   
 });
