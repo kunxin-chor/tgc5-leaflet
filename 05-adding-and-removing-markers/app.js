3e06625bc90b3fc3cdd251d07e9ad934b2884eca
kunxin-chor <chorkunxin@yahoo.com>
2020-03-30 03:59:17 +0000

    Added example for removing markers from a layer

--- 05-adding-and-removing-markers/app.js
+++ 05-adding-and-removing-markers/app.js
@@ -1,9 +1,16 @@
 $(function() {
 
     let map = createMap("map", [1.3521, 103.8198], 13);
-    let group = L.LayerGroup();
-    axios.get('hdb.json').then(function(response){
-       for (let n of nature.data) {
+    let group = L.layerGroup();
+
+    $('#load').click(function(){
+        
+    axios.get('nature.json').then(function(response){
+
+        // empty the layer first, this way no matter how many times you press the button,
+        // the markers won't accumulate
+        group.clearLayers();
+       for (let n of response.data) {
            let marker = L.marker(n.coordinates).bindPopup(`<p>${n.name}</p>`);
            group.addLayer(marker);
        }
@@ -11,4 +18,6 @@ $(function() {
 
     })
     
+    })
+
 });
